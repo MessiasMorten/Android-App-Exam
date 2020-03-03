a50a0e03c079e92c397827be146f00838bd5ab2b
Mikael Åsbjørnsson-Stensland <mikael@Mikaels-MacBook-Pro.local>
2020-03-03 15:50:50 +0100

    UI.

--- app/src/main/java/com/example/lab61/MainActivity.java
+++ app/src/main/java/com/example/lab61/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab61;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout body = findViewById(R.id.body);
+        for (int i = 0; i < 12; i++) {
+            body.addView(new QuestionView(this) {});
+        }
     }
 }
